7c6ba9777673893ac366f331ffbd1568d0dda6ff
Tudor David <tudord1@vmware.com>
2015-07-24 09:15:22 -0700

    added common.h file

---
+++ common/common.h
@@ -0,0 +1,13 @@
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+typedef uint64_t Timestamp;
+typedef uint64_t TimeSpan;
+
+typedef struct TI{
+    Timestamp start;
+    Timestamp end;
+} TimestampInterval;
+
+#endif
