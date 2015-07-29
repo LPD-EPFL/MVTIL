66654f8cfaab2af192e7a2f41a2d825760aef40f
Tudor David <tudord1@vmware.com>
2015-07-28 17:47:51 -0700

    started work on SkipLists

---
+++ server/VersionSkipList.cc
@@ -0,0 +1,14 @@
+#include "VersionSkipList.h"
+
+Version* VersionSkipList::find(Timestamp ts) {
+    int i;
+
+}
+
+
+int VersionSkipList::add(Timestamp ts, Version* v) {
+}
+
+int VersionSkipList::remove(Timestamp ts) {
+    return 0;
+}
---
+++ server/VersionSkipList.h
@@ -0,0 +1,10 @@
+
+#ifndef _VERSION_SKIPLIST_H_
+#define _VERSION_SKIPLIST_H_
+
+typedef struct Node {
+    
+} Node;
+
+
+#endif
