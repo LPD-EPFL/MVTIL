a068288e26f4b3d1af94a07da040dfea2aa4fe38
Tudor David <tudor.david@epfl.ch>
2015-07-28 19:57:51 -0700

    more work on the skip list

--- server/VersionSkipList.cc
+++ server/VersionSkipList.cc
@@ -1,14 +1,84 @@
 #include "VersionSkipList.h"
 
-Version* VersionSkipList::find(Timestamp ts) {
+//returns the first node with timestamp >= ts; also returns its predecessor
+Version* VersionSkipList::find(Timestamp ts, Version* pred) {
     int i;
+    Node* node;
+    Node* next;
+    Version* result = NULL;
 
+    node = head;
+    for (i = node->toplevel-1; i >=0; i--) {
+        next = node->next[i];
+        while (next->timestamp < ts) {
+            node = next;
+            next = node->next[i];
+        }
+    }
+    pred = node->version;
+    node = node->next[0];
+    result = node->version;
+    return result;
 }
 
-
 int VersionSkipList::add(Timestamp ts, Version* v) {
+    int i, l, result;
+    Node* node;
+    Node* next;
+    Node* preds[MAXLEVEL];
+    Node* succs[MAXLEVEL];
+
+    node = head;
+    for (i = node->toplevel-1; i>=0; i--) {
+        next = node->next[i];
+        while (next->timestamp < ts) {
+            node = next;
+            next = node->next[i];
+        }
+        preds[i] = node;
+        succs[i] = node->next[i];
+    }
+    node = node->next[0];
+
+    result = (node->timestamp != ts);
+    if (result == 1) {
+       l = getRandomLevel();
+       node = new Node(ts, v, l); 
+       for (i = 0; i < l; i++) {
+        node->next[i] = succs[i];
+        preds[i]->next[i] = node;
+       }
+    }
+    return result;
 }
 
 int VersionSkipList::remove(Timestamp ts) {
-    return 0;
+    int result = 0;
+    int i;
+    Node* node = NULL;
+    Node* next = NULL;
+    Node* preds[MAXLEVEL];
+    Node* succs[MAXLEVEL];
+
+    node = head;
+    for (i = node->toplevel-1; i >= 0; i--) {
+       next = node->next[i];
+       while (next->timestamp < ts) {
+        node = next;
+        next = node->next[i];
+       }
+       preds[i] = node;
+       succs[i] = node->next[i];
+    }
+
+    if (next->timestamp == ts) {
+        result = 1;
+        for (i = 0; i < head->toplevel; i++) {
+            if (succs[i]->timestamp == ts) {
+                preds[i]->next[i] = succs[i]->next[i];
+            }
+        }
+        delete(next);
+    }
+    return result;
 }
--- server/VersionSkipList.h
+++ server/VersionSkipList.h
@@ -6,5 +6,15 @@ typedef struct Node {
     
 } Node;
 
+class VersionSkiplist {
+    public:
+        Version* find(Timestamp ts, Version* prev);
+        int add(Timestamp ts, Version* v);
+        int remove(Timestamp ts);
+
+    private:
+        Node* head;
+};
+
 
 #endif
