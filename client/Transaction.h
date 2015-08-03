137224c5b018e2cb67e10b9564c154033e74e9e0
Tudor David <tudor.david@epfl.ch>
2015-08-03 11:08:07 -0700

    additions to transaction

--- client/Transaction.h
+++ client/Transaction.h
@@ -1,6 +1,47 @@
 #ifndef _TRANSACTION_H_
 #define _TRANSACTION_H_
 
+#include <vector>
+#include "DataServer_types.h"
+#include "common.h"
+#include "TransactionManager.h"
 
+class Transaction {
+    friend class TransactionManager;
+    public:
+        Transaction();
+        ~Transaction();
+    
+        Value alreadyContains(Key k);
+
+    private:
+        TransactionId transactionId;
+        std::vector<ReadSetEntry> readSet;
+        std::vector<WriteSetEntry> writeSet;
+        std::vector<HintSetEntry> hintSet;
+
+        TimestampInterval currentInterval;
+
+        typedef struct ReadSetEntry {
+            Key k;
+            Value v;
+            TimestampInterval interval;
+            TimestampInterval potential;
+        } ReadSetEntry;
+
+        typedef struct WriteSetEntry {
+            Key k;
+            Value v;
+            TimestampInterval interval;
+            TimestampInterval potential;
+            ServerAddress address;
+        } WriteSetEntry;
+
+        typedef struct HintSetEntry {
+            Key k;
+            TimestampInterval interval;
+            ServerAddress address;
+        } HintSetEntry;
+};
 
 #endif
