76e88be10b00517ea1c29d593996393ecc029322
OVSS <chuangzhetianxia@gmail.com>
2017-07-04 16:22:39 +0200

    Fix small issue

--- client/ServerConnection.h
+++ client/ServerConnection.h
@@ -9,6 +9,7 @@
 #define _SERVER_CONNECTION_H_
 
 #include "client.h"
+#include "server.h"
 #include "DataService.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/transport/TSocket.h>
